f389479231cf32b43c426e6a813be48f56cd8d94
Ludwig <ludi@bobby13.fritz.box>
2022-12-02 08:28:32 +0100

    zustazaufgabe

--- 1/calories.py
+++ 1/calories.py
@@ -1,4 +1,6 @@
 import sys
+from bisect import bisect
+
 
 class Calories:
 
@@ -7,6 +9,8 @@ class Calories:
         self.current_max_elf = None
         self.running_sum = 0
         self.running_elf = 1
+        self.sorted_elves = []
+
 
     def update(self, number):
         self.running_sum += number
@@ -15,9 +19,13 @@ class Calories:
         if not self.current_max_elf:
             self.current_max_elf = 1
             self.current_max = self.running_sum
+            self.sorted_elves = [(self.running_sum, 1)]
             self.running_sum = 0
             self.running_elf = 2
             return
+        elf_tuple = (self.running_sum, self.running_elf)
+        place = bisect(self.sorted_elves, elf_tuple)
+        self.sorted_elves = self.sorted_elves[:place] + [elf_tuple] + self.sorted_elves[place:]
         if self.current_max < self.running_sum:
             self.current_max = self.running_sum
             self.current_max_elf = self.running_elf
@@ -40,4 +48,7 @@ if __name__ == '__main__':
             c.update(val)
         except:
             continue
+    c.next_elf()
     print(f"Elf No {c.current_max_elf} has the most ({c.current_max}) calories in the bag" )
+    top_3_sum = sum([x for (x, _) in c.sorted_elves[-3:]])
+    print(f"the top 3 sum is {top_3_sum}")
--- 1/calories_test.py
+++ 1/calories_test.py
@@ -51,17 +51,54 @@ class TestCalories(unittest.TestCase):
 
 
     def test_dont_update_max(self):
-            c = Calories()
-            c.update(10000)
-            c.next_elf()
-            c.update(1000)
-            c.update(1000)
-            c.next_elf()
-            self.assertEqual(c.current_max, 10000)
-            self.assertEqual(c.current_max_elf, 1)
-            self.assertEqual(c.running_sum, 0)
-            self.assertEqual(c.running_elf, 3)
+        c = Calories()
+        c.update(10000)
+        c.next_elf()
+        c.update(1000)
+        c.update(1000)
+        c.next_elf()
+        self.assertEqual(c.current_max, 10000)
+        self.assertEqual(c.current_max_elf, 1)
+        self.assertEqual(c.running_sum, 0)
+        self.assertEqual(c.running_elf, 3)
 
+    def test_has_sorting(self):
+        c = Calories()
+        self.assertIsNotNone(c.sorted_elves)
+
+    def test_ordered_elves(self):
+        c = Calories()
+        c.update(100)
+        c.next_elf()
+        c.update(200)
+        c.next_elf()
+        c.update(300)
+        c.next_elf()
+        self.assertEqual(c.sorted_elves, [(100, 1), (200, 2), (300, 3)])
+
+    def test_ordered_elves_2(self):
+        c = Calories()
+        c.update(100)
+        c.next_elf()
+        c.update(300)
+        c.next_elf()
+        c.update(200)
+        c.next_elf()
+        self.assertEqual(c.sorted_elves, [(100, 1), (200, 3), (300, 2)])
+
+    def test_ordered_elves_2(self):
+        c = Calories()
+        c.update(6000)
+        c.next_elf()
+        c.update(4000)
+        c.next_elf()
+        c.update(11000)
+        c.next_elf()
+        c.update(24000)
+        c.next_elf()
+        c.update(10000)
+        c.next_elf()
+        self.assertEqual(c.sorted_elves, [(4000, 2), (6000, 1), (10000, 5), (11000, 3), (24000, 4)])
 
 if __name__ == '__main__':
     unittest.main()
