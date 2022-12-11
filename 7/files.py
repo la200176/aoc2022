7f02450159e96f61e5e72359f3aca80e37ff8dae
Ludwig <ludi@bobby13.fritz.box>
2022-12-11 12:34:51 +0100

    more chaos

--- 7/files.py
+++ 7/files.py
@@ -24,41 +24,132 @@ $ ls
 5626152 d.ext
 7214296 k"""
 
-def process_input(line, location):
-    (dollar, cmd, *args) = line.split()
-    if cmd == "cd":
-        (where, ) = args
-        if where == '..':
-            return (location[:-1], None)
-        else:
-            return (location + args, None)
-    elif cmd == "ls":
-        return (location, None)
-    raise ValueError(f"cant process '{line}'")
+list1 = """4060174 j
+8033020 d.log
+5626152 d.ext
+7214296 k"""
 
-def next_or_last_line(f, last_line=None):
-    if last_line:
-        return last_line
-    line = f.readline()
-    if line:
-        return line.strip()
+list2 = """dir e
+29116 f
+2557 g
+62596 h.lst
+$ cd e
+"""
 
-def test():
-    sio = io.StringIO(example)
-    location = []
-    old_line = None
+list3 = """dir a
+14848514 b.txt
+8504156 c.dat
+dir d
+"""
+
+cmd1 = """$ cd /
+$ ls"""
+
+
+def parse_listing_line(line):
+    try:
+        (x, y) = line.strip().split()
+        if x == 'dir':
+            return ((x, y), True)
+        return (('file', int(x), y), True)
+    except ValueError:
+        return (None, False)
+
+
+def read_listing(f, parse_line, location=[]):
+    result = []
     while True:
-        line = next_or_last_line(sio, old_line)
+        line = f.readline()
+        if not line:
+            return (result, None)
+        (thing, ok) = parse_line(line)
+        if not ok:
+            f.unread(line)
+            new_location = read_command(f, parse_command_line, location)
+            print(f"LIS {new_location} {result}")
+        else:
+            result.append(thing)
+
+
+def parse_command_line(line):
+    (dollar, cmd, *rest) = line.strip().split()
+    if dollar == "$":
+        if cmd == "cd":
+            (target,) = rest
+            if target == "..":
+                return (lambda x: x[:-1], True)
+            else:
+                return (lambda x: x + rest, True)
+        if cmd == "ls":
+            return (lambda x: x, True)
+    return (None, False)
+
+
+def read_command(f, parse_command_line, location=[]):
+    while True:
+        line = f.readline()
+        if not line:
+            return (location, None)
+        (navigation, ok) = parse_command_line(line)
+        if not ok:
+            f.unread(line)
+            listing = read_listing(f, parse_listing_line, location)
+            print(f"CMD {location} {listing}")
+        else:
+            location = navigation(location)
+
+
+class Unread:
+    def __init__(self, stream):
+        self.stream = stream
+        self.unread_line = None
+
+    def unread(self, line):
+        self.unread_line = line
+
+    def readline(self):
+        if self.unread_line:
+            unread_line = self.unread_line
+            self.unread_line = None
+            return unread_line
+        return self.stream.readline()
+
+
+def reader(f):
+    sio = Unread(f)
+    processor = dict(listing=lambda f: read_listing(f, parse_listing_line),
+                     command=lambda f: read_command(f, parse_command_line))
+    mode = "command"
+    while True:
+        (result, line) = processor[mode](sio)
         if not line:
             break
-        try:
-            (new_location, old_line) = process_input(line, location)
-        except ValueError as ve:
-            msg = f"ERR {ve}"
-        else:
-            msg = "OK"
-            location = new_location
-        print(f"{line.ljust(30)} #{location} {msg}")
+        print(f"{mode}: {result}")
+        sio.unread(line)
+        if mode == "command":
+            mode = "listing"
+        elif mode == "listing":
+            mode = "command"
 
 
-test()
+def test_listing(x):
+    sio = Unread(io.StringIO(x))
+    print(read_listing(sio, parse_listing_line))
+
+
+def test_cmd(x):
+    sio = Unread(io.StringIO(x))
+    print(read_command(sio, parse_command_line))
+
+
+def test_unread():
+    sio = Unread(io.StringIO("abc\ndef\n123"), "blabla\n")
+    while True:
+        line = sio.readline()
+        if not line:
+            break
+        print(line.strip())
+
+def test_reader():
+    sio = Unread(io.StringIO(example))
+    read_command(sio, parse_command_line, [])
