52b705789c37eea43d053b47eebb8a99f7754cc3
Ludwig <ludi@bobby13.fritz.box>
2022-12-15 09:28:04 +0100

    day 8, start

---
+++ 8/trees.py
@@ -0,0 +1,26 @@
+example = """30373
+25512
+65332
+33549
+35390"""
+
+
+def to_map(s):
+    return [[int(x) for x in list(row)] for row in s.split('\n')]
+
+
+def visible(trees, position):
+    width = len(trees[0])
+    height = len(trees)
+    (row, col) = position
+    tree_height = trees[row][col]
+    left_trees = trees[row][:col]
+    right_trees = trees[row][col+1:]
+    top_trees = [trees[row][col] for row in range(0, row)]
+    bottom_trees = [trees[row][col] for row in range(row+1, height)]
+    smaller = lambda x: x < tree_height
+    all_smaller = lambda trees: all([smaller(tree) for tree in trees])
+    return dict(left=(left_trees, all_smaller(left_trees)),
+                right=(right_trees, all_smaller(right_trees)),
+                top=(top_trees, all_smaller(top_trees)),
+                bottom=(bottom_trees, all_smaller(bottom_trees)))
