71e3cd61b091e684bb2f4fdc13314dfd63fbad69
Ludwig <ludi@bobby13.fritz.box>
2022-12-17 14:23:45 +0100

    day 8

--- 8/trees.py
+++ 8/trees.py
@@ -46,3 +46,34 @@ def task1():
         for (col, _) in enumerate(xx):
             count += int(visible(trees, [row, col]))
     return count
+
+def view_score(tree_height, row):
+    count = 0
+    for (n, x) in enumerate(row):
+        count += 1
+        if x >= tree_height:
+            break
+    return count
+
+def tree_score(trees, position):
+    details = visible_details(trees, position)
+    (row, col) = position
+    tree_height = trees[row][col]
+    result = dict()
+    for key, direction in (('left', -1), ('right', 1), ('top', -1), ('bottom', 1)):
+        (tree_row, visible) = details[key]
+        score = view_score(tree_height, tree_row[::direction])
+        result[key] = (tree_row, visible, score)
+    return result
+
+def task2():
+    with open('input') as f:
+        s = f.read()
+    trees = to_map(s)
+    max_ts = 0
+    for (row, xx) in enumerate(trees):
+        for (col, _) in enumerate(xx):
+            ts_details = tree_score(trees, (row, col))
+            ts = ts_details['left'][2] * ts_details['right'][2] * ts_details['top'][2] * ts_details['bottom'][2]
+            max_ts = max(ts, max_ts)
+    return max_ts
